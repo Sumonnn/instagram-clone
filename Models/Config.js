267bb88ff8a5e9f6912425baad42fd6ede22c79f
Sumon <sumonkhan1081@gmail.com>
2024-01-08 23:18:48 +0530

    deploy testing

--- Models/Config.js
+++ Models/Config.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 require('dotenv').config()
 const url = process.env.DATABASE_URL;
 
 mongoose
-    .connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(url)
     .then(() => console.log("DB connected"))
     .catch((err) => console.log(err))
 
